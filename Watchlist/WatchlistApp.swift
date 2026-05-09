d05103e64b28d8bcb579d7aabf83e419f91e8dd8
Jasmine <sandhujasmine@seasiainfotech.com>
2026-05-09 19:12:16 +0530

    Initial setup

---
+++ Watchlist/WatchlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Jasmine Kaur on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
