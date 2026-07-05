1cdadb9a7a227a0849e1e32a599fe25c29db97d1
Jasmine <sandhujasmine@seasiainfotech.com>
2026-07-05 18:19:45 +0530

    form added

---
+++ Watchlist/Component/ButtonImageView.swift
@@ -0,0 +1,29 @@
+//
+//  WatchlistApp.swift
+//  Watchlist
+//
+//  Created by Jasmine Kaur on 05/07/26.
+//
+
+import SwiftUI
+
+struct ButtonImageView: View {
+  let symbolName: String
+  
+  var body: some View {
+    Image(systemName: symbolName)
+      .resizable()
+      .scaledToFit()
+      .foregroundStyle(.blue.gradient)
+      .padding(8)
+      .background(
+        Circle()
+          .fill(.ultraThickMaterial)
+      )
+      .frame(width: 80)
+  }
+}
+
+#Preview {
+  ButtonImageView(symbolName: "plus.circle.fill")
+}
--- Watchlist/View/EmptyListView.swift
+++ Watchlist/View/EmptyListView.swift
@@ -9,19 +9,35 @@ import SwiftUI
 
 struct EmptyListView: View {
     var body: some View {
-        Circle()
-            .fill(.blue.gradient)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
-            .frame(width: 180)
-            .overlay {
-                Image(systemName: "movieclapper")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:100)
-                    .foregroundStyle(Color(UIColor.systemBackground))
+        ContentUnavailableView {
+            Circle()
+                .fill(.blue.gradient)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
+                .frame(width: 180)
+                .overlay {
+                    Image(systemName: "movieclapper")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:100)
+                        .foregroundStyle(Color(UIColor.systemBackground))
+                }
+                .padding(.bottom, 90)
+        } description: {
+            GroupBox {
+                TabView {
+                    PageTabView(icon: "1.circle", description: "Add movies to watchlist if you wish to wath them later")
+                        .padding(.bottom, 36)
+                    PageTabView(icon: "2.circle", description: "You can later on watch any random moview from the list")
+                        .padding(.bottom, 36)
+
+                }
+                .tabViewStyle(PageTabViewStyle())
+                .frame(minWidth: 0, maxWidth: 560, minHeight: 120, maxHeight: 180)
             }
+        }
+        
     }
 }
 
