14ac8ed926e04e7271db56562180fedd452c3e11
Jasmine <sandhujasmine@seasiainfotech.com>
2026-07-05 12:44:51 +0530

    reusable views created

---
+++ Watchlist/View/EmptyListView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyListView.swift
+//  Watchlist
+//
+//  Created by Jasmine Kaur on 05/07/26.
+//
+
+import SwiftUI
+
+struct EmptyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyListView()
+}
