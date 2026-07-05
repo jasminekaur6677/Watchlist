7d313f279c2ceea852161a671c70d8ba154f1faf
Jasmine <sandhujasmine@seasiainfotech.com>
2026-07-05 12:57:42 +0530

    re-usable view creadted

--- Watchlist/View/EmptyListView.swift
+++ Watchlist/View/EmptyListView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct EmptyListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .fill(.blue.gradient)
+            .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
+            .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
+            .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
+            .frame(width: 180)
+            .overlay {
+                Image(systemName: "movieclapper")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:100)
+                    .foregroundStyle(Color(UIColor.systemBackground))
+            }
     }
 }
 
--- Watchlist/View/PageTabView.swift
+++ Watchlist/View/PageTabView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct PageTabView: View {
+    var icon: String
+    var description: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(spacing: 8) {
+            Image(systemName: icon)
+                .imageScale(.large)
+                .font(.largeTitle.weight(.light))
+                .symbolEffect(.breathe)
+            Text(description)
+                .font(.title.weight(.light))
+                .fontWidth(.compressed)
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
 #Preview {
-    PageTabView()
+    PageTabView(icon: "1.circle", description: "Add some movies to the watchlist if you plan to watch later")
+        .padding()
 }
